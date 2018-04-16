2deb09c2e41b099b1c3a5a08c7195e3da126a51a
HshengX <897151238@qq.com>
2018-04-16 16:05:21 +0800

    Sample10_3 任意剪裁平面

---
+++ Sample10_3/js/loadch.js
@@ -0,0 +1,31 @@
+  	function loadchObjFile(url)
+		{
+		    var req = new XMLHttpRequest();
+		    req.onreadystatechange = function () { processBallLoadObj(req) };
+		    req.open("GET", url, true);
+		    req.responseType = "text";
+		    req.send(null);
+		}
+		
+		function createBall(objDataIn)
+		{
+		   if(shaderProgArray[0])
+		   {
+		      //创建绘制用的物体
+          ooTri=new ObjObject(gl,objDataIn.vertices,objDataIn.normals,objDataIn.texcoords,shaderProgArray[0]); 
+		   }
+		   else
+		   {
+		      setTimeout(function(){createBall(objDataIn);},10); //定时调用createBall（）方法，只调用一次 
+		   }
+		}
+		
+		function processBallLoadObj(req)
+		{
+		    if (req.readyState == 4) 
+		    {
+		        var objStr = req.responseText;	       
+		        var dataTemp=fromObjStrToObjectData(objStr);	
+		        createBall(dataTemp);                     
+		    }
+		} 
